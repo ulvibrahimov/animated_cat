a57ff8d941ca0339a63914779134d808a7c5d564
ulvibrahimov <ulvi11112008@gmail.com>
2026-02-25 08:21:27 +0400

    Dashboard ve vahid struktur elave edildi

--- app/src/main/java/com/example/myapplication56/Animation_Screen.kt
+++ app/src/main/java/com/example/myapplication56/Animation_Screen.kt
@@ -1,81 +1,81 @@
-package com.example.myapplication56
-
-import android.graphics.Color
-import android.hardware.Sensor
-import android.hardware.SensorEvent
-import android.hardware.SensorEventListener
-import android.hardware.SensorManager
-import android.os.Bundle
-import android.speech.tts.TextToSpeech
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import java.util.EnumSet.of
-import java.util.Locale
-
-class Animation_Screen : AppCompatActivity(), SensorEventListener {
-
-    private lateinit var sensorManager: SensorManager
-
-    private lateinit var proxSensor : Sensor
-
-    private lateinit var layout : ConstraintLayout
-    private lateinit var textToSpeech: TextToSpeech
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_animation_screen)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-        textToSpeech = TextToSpeech(this){ status ->
-            if(status!= TextToSpeech.ERROR){
-                textToSpeech.language = Locale.ENGLISH
-            }
-
-        }
-        layout = findViewById(R.id.main)
-        sensorManager = getSystemService(SENSOR_SERVICE)as SensorManager
-        proxSensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY)!!
-
-
-
-    }
-
-    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
-    }
-
-    override fun onSensorChanged(event: SensorEvent?) {
-        val value = event?.values?.get(0) ?: 0f
-
-        when(event?.sensor?.type){
-            Sensor.TYPE_PROXIMITY ->{
-                if (value < (proxSensor.maximumRange)){
-                    layout.setBackgroundColor(Color.GREEN)
-                    textToSpeech.speak("I can see you in the dark", TextToSpeech.QUEUE_FLUSH,null,null)
-                }else{
-                    layout.setBackgroundColor(Color.BLACK)
-
-
-
-                }
-            }
-        }
-    }
-
-    fun speech(text: String) {
-        textToSpeech.setPitch(2.0f)
-        textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null, null)
-    }
-
-    override fun onResume() {
-        proxSensor.let {sensorManager
-            .registerListener(this,it, SensorManager.SENSOR_DELAY_NORMAL)  }
-        super.onResume()
-    }
-}
+//package com.example.myapplication56
+//
+//import android.graphics.Color
+//import android.hardware.Sensor
+//import android.hardware.SensorEvent
+//import android.hardware.SensorEventListener
+//import android.hardware.SensorManager
+//import android.os.Bundle
+//import android.speech.tts.TextToSpeech
+//import androidx.activity.enableEdgeToEdge
+//import androidx.appcompat.app.AppCompatActivity
+//import androidx.constraintlayout.widget.ConstraintLayout
+//import androidx.core.view.ViewCompat
+//import androidx.core.view.WindowInsetsCompat
+//import java.util.EnumSet.of
+//import java.util.Locale
+//
+//class Animation_Screen : AppCompatActivity(), SensorEventListener {
+//
+//    private lateinit var sensorManager: SensorManager
+//
+//    private lateinit var proxSensor : Sensor
+//
+//    private lateinit var layout : ConstraintLayout
+//    private lateinit var textToSpeech: TextToSpeech
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        enableEdgeToEdge()
+//        setContentView(R.layout.activity_animation_screen)
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+//        textToSpeech = TextToSpeech(this){ status ->
+//            if(status!= TextToSpeech.ERROR){
+//                textToSpeech.language = Locale.ENGLISH
+//            }
+//
+//        }
+//        layout = findViewById(R.id.main)
+//        sensorManager = getSystemService(SENSOR_SERVICE)as SensorManager
+//        proxSensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY)!!
+//
+//
+//
+//    }
+//
+//    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+//    }
+//
+//    override fun onSensorChanged(event: SensorEvent?) {
+//        val value = event?.values?.get(0) ?: 0f
+//
+//        when(event?.sensor?.type){
+//            Sensor.TYPE_PROXIMITY ->{
+//                if (value < (proxSensor.maximumRange)){
+//                    layout.setBackgroundColor(Color.GREEN)
+//                    textToSpeech.speak("I can see you in the dark", TextToSpeech.QUEUE_FLUSH,null,null)
+//                }else{
+//                    layout.setBackgroundColor(Color.BLACK)
+//
+//
+//
+//                }
+//            }
+//        }
+//    }
+//
+//    fun speech(text: String) {
+//        textToSpeech.setPitch(2.0f)
+//        textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null, null)
+//    }
+//
+//    override fun onResume() {
+//        proxSensor.let {sensorManager
+//            .registerListener(this,it, SensorManager.SENSOR_DELAY_NORMAL)  }
+//        super.onResume()
+//    }
+//}
